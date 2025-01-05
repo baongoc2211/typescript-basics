7c8ab6e5f0f88b5e0504ea67921bb306942f70ad
baongoc2211 <phangiangbaongoc9a2@gmail.com>
2025-01-05 22:50:25 +0700

    init project

---
+++ indes.ts
@@ -0,0 +1,72 @@
+//1//Strings
+// let myName: string = "Bao Ngoc";
+// myName = "something other person";
+// console.log(myName);
+
+// // Numbers
+// let bavNumber: number = 12;
+// console.log(bavNumber);
+
+// //Boolean
+// let tsHard: boolean = false;
+// tsHard = 12;
+// console.log(tsHard);
+
+//2// let tech = "TypeScript";
+// let bavNumber = 8;
+// let tsHard = true;
+// ("");
+// // In ra dữ liệu của parameotor
+// // console.log(tech);
+// // console.log(bavNumber);
+// // console.log(tsHard);
+
+// // In ra kiểu dữ liệu của parametor đó
+// console.log(typeof tech);
+// console.log(typeof bavNumber);
+// console.log(typeof tsHard);
+
+//3// có thể có nhiều kiểu dữ liệu nếu có một kiểu dữ liệu
+// thì thay any thành kiểu dữ liệu mong muốn ví dụ string, number
+// let color: any = "crimson";
+// color = 20;
+// color = true;
+// color();
+// console.log(color);
+
+//4// Regular Function
+// function addOne(num: number) {
+//   return num + 1;
+// }
+// const res = addOne(3);
+// console.log(res);
+
+//5//Arrow Func
+// const double = (x: number, y: number) => x * y;
+// const res = double(2, 10);
+// console.log(res);
+
+//6//
+// function greet(person: string = "Anonoymous") {
+//   return "Hello ${person}";
+// }
+// const res = greet("BaoNgoc");
+// console.log(res);
+
+//7// Regular Func
+// function double1(x: number) {
+//   return x * x;
+// }
+// const res = double1(2);
+// console.log(res);
+
+//8//Arrow Func
+// const doubl2 = (x: number): number => x * x;
+// console.log(doubl2(2));
+
+//
+// function printMessage(message: string): void {
+//   console.log(`This is my ${message}`);
+//   return message;
+// }
+// printMessage("message");
